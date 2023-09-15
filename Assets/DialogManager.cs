e71a8fd2033b33eb122728490867de54eb2e0141
AdriUlima <20200711@aloe.ulima.edu.pe>
2023-09-15 17:38:51 -0500

    Mejoras al diálogo

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -10,6 +10,8 @@ public class DialogManager : MonoBehaviour
     [SerializeField] private GameObject panel;
     private string _targetText;
     private float _charCount;
+    private List<string> Dialogos = new();
+    private bool isWriting;
 
     private void Start()
     {
@@ -20,18 +22,44 @@ public class DialogManager : MonoBehaviour
     {
         if (panel.activeSelf)
         {
+            if (txtMessage.text == _targetText)
+            {
+                isWriting = false;
+            }
             _charCount = Mathf.Min(_charCount + Time.deltaTime * 60, _targetText.Length);
             txtMessage.text = _targetText.Substring(0, (int)_charCount);
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            Hide();
+            if (Dialogos.Count <= 0 && !isWriting)
+            {
+                Hide();
+            }
+            else if(Dialogos.Count > 0)
+            {
+                ShowText();
+            }
+            else
+            {
+                txtMessage.text = _targetText;
+            }
         }
     }
 
-    public void ShowText (string text)
+    public void ShowText ()
     {
+        isWriting = true;
+        string text;
+        if (Dialogos.Count > 0)
+        {
+            text = Dialogos[0];
+            Dialogos.RemoveAt(0);
+        }
+        else
+        {
+            text = "ERROR. NO HAY TEXTO ASIGNADO";
+        }
         _charCount = 0;
         Show();
         _targetText = text;
@@ -45,5 +73,18 @@ public class DialogManager : MonoBehaviour
     public void Hide ()
     {
         panel.SetActive(false);
+    }
+
+    public void SetDialogos(string[] dialogos)
+    {
+        if (dialogos != null)
+        {
+            Dialogos.Clear();
+            for(int i = 0; i < dialogos.Length; i++)
+            {
+                Dialogos.Add(dialogos[i]);
+            }
+        }
+        ShowText();
     }
 }
--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Interactable : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _sr;
+    public string[] Dialogos;
+    
     public void Interact ()
     {
-        DialogManager.Instance.ShowText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur a erat nam at lectus urna.");
+        DialogManager.Instance.SetDialogos(Dialogos);
     }
 
     public void Highlight ()
