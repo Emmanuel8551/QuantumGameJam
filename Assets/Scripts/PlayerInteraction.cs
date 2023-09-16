6e961ca4a57575ae56e0c1f361763219054f2d17
ManuElCsm <a20190702@pucp.edu.pe>
2023-09-16 12:26:00 -0500

    Interact Button Icon

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private float _interactRadius;
+    [SerializeField] private GameObject _interactKeyIcon;
     private Interactable _lastInteractable;
     private Player _player;
+    private bool _isInteracting;
 
     private void Start()
     {
@@ -18,28 +20,44 @@ public class PlayerInteraction : MonoBehaviour
     {
         if (!_player.IsControlling)
         {
+            _isInteracting = false;
             _lastInteractable?.UnHighlight();
-            return;
+        }
+        else
+        {
+            Interactable interactable = GetInteractedObject();
+            if (interactable != null)
+            {
+                _lastInteractable = interactable;
+                interactable.Highlight();
+                _isInteracting = true;
+            }
+            else
+            {
+                _isInteracting = false;
+                _lastInteractable?.UnHighlight();
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                interactable.Interact();
+            }
         }
 
+        _interactKeyIcon.SetActive(_isInteracting);
+    }
+
+    private Interactable GetInteractedObject ()
+    {
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, _interactRadius, Vector2.zero);
         if (hit.collider != null)
         {
             if (hit.collider.gameObject.TryGetComponent(out Interactable interactable))
             {
-                _lastInteractable = interactable;
-                interactable.Highlight();
-
-                if (Input.GetKeyDown(KeyCode.E))
-                {
-                    interactable.Interact();
-                }
+                return interactable;       
             }
         }
-        else
-        {
-            _lastInteractable?.UnHighlight();
-        }
+        return null;
     }
 
     private void OnDrawGizmos()
