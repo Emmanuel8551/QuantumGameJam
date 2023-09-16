76811b8afac051abccf99df43b3bd795b949003b
ManuElCsm <a20190702@pucp.edu.pe>
2023-09-16 15:42:20 -0500

    Project Refactor

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadLevel ()
+    {
+
+    }
+}
+
+[System.Serializable]
+public class Level
+{
+
+}
