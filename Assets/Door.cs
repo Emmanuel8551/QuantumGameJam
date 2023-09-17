c1298bab01dd5f0f11f3673e638f98c5f4120c8e
ManuElCsm <a20190702@pucp.edu.pe>
2023-09-16 23:08:52 -0500

    Door Connection

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _sr;
+    public static List<Door> Doors = new List<Door>();
     [SerializeField] private int nextRoomIndex;
     [SerializeField] private float _detectRadius;
     [SerializeField] private LayerMask _targetMask;
+    [SerializeField] private int _doorIndex;
+    [SerializeField] private int _targetIndex;
+
+    private void OnEnable()
+    {
+        Doors.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        Doors.Remove(this);
+    }
 
     private void Update()
     {
@@ -18,6 +30,14 @@ public class Door : MonoBehaviour
             if (hit.collider.gameObject.TryGetComponent(out Player player))
             {
                 LevelLoader.Instance.ChangeRoomPair(nextRoomIndex);
+                Door targetDoor = Doors.Find(d => d._doorIndex == _targetIndex);
+                if (targetDoor != null)
+                {
+                    Vector3 diff = targetDoor.transform.position - player.transform.position;
+                    Player.PlayerPast.transform.position += diff;
+                    Player.PlayerFuture.transform.position += diff;
+                }
+
             }
         }
     }
